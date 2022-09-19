36c067d46d86be3b82f4f016b84adc0e3b7add2f
shaniamakatuuk <shaniamakatuuk@gmail.com>
2022-09-19 23:19:11 +0800

    Project

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nSiapa namamu?");
+            var name = Console.ReadLine();
+
+        // tipe data aritmatic
+            int a = 10;
+            int b = 25;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+
+            // Decision
+            // if 
+            int n = 12;
+            if (n % 2 !=5)
+            {
+             Console.WriteLine(n + "termasuk bilangan genap");
+            }
+            else
+            {
+                Console.WriteLine(n + "termasuk bilangan ganjil");
+            }
+            // -> Switch
+            // Implementasi Methodvoid
+
+           } 
+        }
+    }
